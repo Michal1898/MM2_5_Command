cef997506fe3d205dd2f9f61e3d26d3dc88a0326
Michal1898 <michal.volf@gmail.com>
2022-12-27 11:15:56 +0100

    Class Attempt modified

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ code_values = [_ for _ in range(1, 9)]
 
 
 class Attempt:
-    def __init__(self, your_code="0 0 0 0 0", no_of_digits=5):
-        your_code = your_code.split()
-        your_code = [int(a) for a in your_code]
-        print(f"Your code: {your_code}, {no_of_digits}")
+    def __init__(self, att_no, your_code=[], white=0, black=0 ):
+
+        self.__index = att_no
+        self.__your_code=your_code
+        self.__black_stick = white
+        self.__white_stick = black
+
+    def __repr__(self):
+        self.att_Report = ""
+        self.att_Report+= f"{self.__index}. "
+        self.att_Report+= f"{self.__your_code} "
+        self.att_Report+= f"Black: {self.__black_stick} "
+        self.att_Report+= f"White: {self.__white_stick} "
+        self.att_Report+="\n"
+        return self.att_Report
 
 class MasterMind:
     def __init__(self, attempt=10, option=8, digit=5):
@@ -41,6 +52,20 @@ class MasterMind:
 
     def list_of_values(self):
         return self.__possible_values
+
+    def next_attempt(self, your_attempt ="0 0 0 0 0"):
+        #self.__game_active=False
+        #self.__current_att=9
+        if self.__game_active == False:
+            return False, "Game is not active!"
+
+        elif self.__current_att<self.__attempt:
+            the_attempt=Attempt(self.__current_att, your_attempt, 3 , 1)
+            return True, the_attempt
+
+
+        else:
+            return False, "You have not attempt available!"
 
     def __repr__(self):
         MM_Report = ""
@@ -103,9 +128,12 @@ match current_game:
         # the_game.show_secret()
         # print(the_game.list_of_values())
         print(repr(the_game))
+        single_attempt = the_game.next_attempt([1, 2, 3, 4, 5])
+        print(single_attempt)
 
-        the_attempt = Attempt("1 2 3 4 5", 13)
-        the_attempt2 = Attempt("7 7 7 13 13 13", 17)
+        the_attempt = Attempt(13, [1 ,2 ,3 ,3 ,2], 13, 7)
+        print(the_attempt)
+        # the_attempt2 = Attempt("7 7 7 13 13 13", 17)
         # the_game = MasterMind(7,10,4)
         # the_game.show_secret()
         # print(the_game.list_of_values())
@@ -127,8 +155,8 @@ match current_game:
 print(current_game)
 
 # quit game?
-next_game = str.lower((input("Dalsi kolo? (Ano/Ne)")))
-print(next_game)
+#next_game = str.lower((input("Dalsi kolo? (Ano/Ne) ")))
+#print(next_game)
 
 
 print("Game over!")
